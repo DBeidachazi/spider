b9c2f864d92aa5afd7700f5fc68ffc65e88e44df
Zixuan Dong <269502169@qq.com>
2026-02-12 09:58:19 +0800

    init

---
+++ mainwindow.cpp
@@ -0,0 +1,15 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setWindowTitle("这是你自己");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
